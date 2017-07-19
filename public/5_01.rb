f612d3f6eb710ca664fbd116db0531f2aa7580d1
Alfons Rovira <inclusa@gmail.com>
2017-07-19 04:16:46 +0200

    Incorporada carpeta public

---
+++ public/5_01.rb
@@ -0,0 +1,5 @@
+paraules = {thin: 'prim', thing: 'cosa', sun: 'sol', win: 'guanyar', wing: 'ala', ran: 'correr en passat', rang: 'rang', singer: 'cantant', sinner: 'pecador'}
+
+paraules.sort.each do |clau,valor|
+  puts "#{clau}: #{valor}"
+end
